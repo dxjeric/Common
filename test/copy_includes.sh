3d224d26f9151e17a914229d4d343bc429412f7d
Eric-Dang <frederick.dang@gmail.com>
2015-08-24 15:23:39 +0800

    modify config file after files name changed

--- test/copy_includes.sh
+++ test/copy_includes.sh
@@ -2,5 +2,5 @@
 # copy headers
 # ------------------------------------------------------------------------
 echo "copy public headers"
-cp ../src/log.h ./log.h
+cp ../src/liblog.h ./liblog.h
 cp ../src/log_config.h ./log_config.h
